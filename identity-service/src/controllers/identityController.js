b504faac59b256e384a52956e6111377a24e3847
jchiu21 <jamiechiu21@gmail.com>
2025-09-13 11:59:07 -0400

    Finished server.js setup

--- identity-service/src/controllers/identityController.js
+++ identity-service/src/controllers/identityController.js
@@ -10,7 +10,9 @@ const registerUser = async (req, res) => {
     const { error } = validateRegistration(req.body);
     if (error) {
       // use .warn() for client-side errors (expected application behavior)
-      logger.warn("Validation error", error.details[0].message);
+      logger.warn("Validation error", {
+        validationMessage: error.details[0].message,
+      });
       return res.status(400).json({
         success: false,
         message: error.details[0].message,
--- identity-service/src/server.js
+++ identity-service/src/server.js
@@ -6,8 +6,13 @@ const helmet = require("helmet");
 const cors = require("cors");
 const { RateLimiterRedis } = require("rate-limiter-flexible");
 const Redis = require("ioredis");
+const { rateLimit } = require("express-rate-limit");
+const { RedisStore } = require("rate-limit-redis"); // redis store for express-rate-limit
+const routes = require("./routes/identityService");
+const errorHandler = require("./middleware/errorHandler");
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 // connect to mongo
 mongoose
@@ -25,10 +30,12 @@ app.use(express.json());
 // logger middleware
 app.use((req, res, next) => {
   logger.info(`Recieved ${req.method} request to ${req.url}`);
-  logger.info(`Request body, ${req.body}`);
+  logger.info("Request body", req.body);
   next();
 });
 
+// rate limiter applied globally for all requests
+// ddos / brute force protection for all endpoints
 const rateLimiter = new RateLimiterRedis({
   storeClient: redisClient, // connection to redis server
   keyPrefix: "middleware", // namespace for the keys in redis
@@ -48,4 +55,40 @@ app.use((req, res, next) => {
         message: "Too many requests",
       });
     });
+});
+
+// sensitive endpoint rate limiter (protects against abuse of signup/login routes)
+const sensitiveEndpointsLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 50,
+  standardHeaders: true,
+  legacyHeaders: false,
+  handler: (req, res) => {
+    logger.warn(`Sensitive endpoint rate limit exceeded for IP: ${req.ip}`);
+    res.status(429).json({
+      success: false,
+      message: "Too many requests",
+    });
+  },
+  // by default stores request counts in memory, we can use Redis
+  store: new RedisStore({
+    // function used to forward commands to redis
+    sendCommand: (...args) => redisClient.call(...args),
+  }),
+});
+
+app.use("/api/auth/register", sensitiveEndpointsLimiter);
+
+// routes
+app.use("/api/auth", routes);
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  logger.info(`Identity service running on port ${PORT}`);
+});
+
+// triggers if promise fails reject() or throw inside async but is never caught
+process.on("unhandledRejection", (reason, promise) => {
+  logger.error("Unhandled rejection at %o. Reason: %s", promise, reason);
 });
