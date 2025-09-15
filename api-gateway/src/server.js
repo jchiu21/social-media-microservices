2928fb37a7a46c70d6203e858c6d0ecdb2faa453
jchiu21 <jamiechiu21@gmail.com>
2025-09-15 05:23:54 -0400

    Setup api gateway and proxy

---
+++ api-gateway/src/server.js
@@ -0,0 +1,91 @@
+require("dotenv").config({ quiet: true });
+const express = require("express");
+const cors = require("cors");
+const Redis = require("ioredis");
+const helmet = require("helmet");
+const { rateLimit } = require("express-rate-limit");
+const { RedisStore } = require("rate-limit-redis");
+const logger = require("./utils/logger");
+const proxy = require("express-http-proxy");
+const errorHandler = require("./middleware/errorHandler");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const redisClient = new Redis(process.env.REDIS_URL);
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+// logger middleware
+app.use((req, res, next) => {
+  logger.info(`Recieved ${req.method} request to ${req.url}`);
+  logger.info("Request body", req.body);
+  next();
+});
+
+// rate limiting
+const ratelimit = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+  handler: (req, res) => {
+    logger.warn(`Sensitive endpoint rate limit exceeded for IP: ${req.ip}`);
+    res.status(429).json({
+      success: false,
+      message: "Too many requests",
+    });
+  },
+  // by default stores request counts in memory, we can use Redis
+  store: new RedisStore({
+    // function used to forward commands to redis
+    sendCommand: (...args) => redisClient.call(...args),
+  }),
+});
+app.use(ratelimit);
+
+const proxyOptions = {
+  proxyReqPathResolver: (req) => {
+    // /v1/users/login -> /api/users/login
+    return req.originalUrl.replace(/^\/v1/, "/api");
+  },
+  proxyErrorHandler: (err, res, next) => {
+    logger.error(`Proxy error ${err.message}`);
+    res.status(500).json({
+      message: `Internal server error`,
+      err: err.message,
+    });
+  },
+};
+
+// forward any request starting with /v1/auth to the identity service
+app.use(
+  "/v1/auth",
+  proxy(process.env.IDENTITY_SERVICE_URL, {
+    ...proxyOptions,
+    // modify outgoing request to identity service
+    proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+      proxyReqOpts.headers["content-type"] = "application/json"; // ensures content-type is application/json
+      return proxyReqOpts;
+    },
+    // runs after proxy recieves response for identity service, before it sends response back to original client
+    userResDecorator: (proxyRes, proxyResData, userReq, userRes) => {
+      logger.info(
+        `Response recieved from Identity service ${proxyRes.statusCode}`
+      );
+      return proxyResData;
+    },
+  })
+);
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  logger.info(`API Gateway is running on port ${PORT}`);
+  logger.info(
+    `Identity service is running on port ${process.env.IDENTITY_SERVICE_URL}`
+  );
+  logger.info(`Redis URL ${process.env.REDIS_URL}`);
+});
---
+++ api-gateway/src/utils/logger.js
@@ -0,0 +1,32 @@
+const winston = require("winston");
+
+const logger = winston.createLogger({
+  // determine the minimum importance of messages the logger will output
+  // -> in prod will log info, warn, error messages (avoid noisy debug logs)
+  // -> in dev will log everything from debug level up
+  level: process.env.NODE_ENV === "production" ? "info" : "debug",
+  // merge multiple format steps into one pipeline
+  format: winston.format.combine(
+    winston.format.timestamp(), // add timestamp to every log
+    winston.format.errors({ stack: true }), // include full error stack trace
+    winston.format.splat(), // enables string interpolation
+    winston.format.json() // outputs every log as JSON object
+  ),
+  defaultMeta: { service: "api-gateway" }, // add default metadata to identify service
+  // transports are where the logs should be sent
+  transports: [
+    // 1. console transport - outputs to terminal
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      ),
+    }),
+    // 2. file transport - saves only ERROR logs to a file
+    // 3. saves all logs (info and above) to a file
+    new winston.transports.File({ filename: "error.log", level: "error" }),
+    new winston.transports.File({ filename: "combined.log" }),
+  ],
+});
+
+module.exports = logger;
