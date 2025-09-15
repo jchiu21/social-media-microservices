df27a717a1d6b754afd08b164e8b1d02ed37d667
jchiu21 <jamiechiu21@gmail.com>
2025-09-15 06:37:00 -0400

    Implemented refresh token and logout routes and controllers

--- identity-service/src/controllers/identityController.js
+++ identity-service/src/controllers/identityController.js
@@ -1,7 +1,8 @@
 const User = require("../models/User");
 const generateTokens = require("../utils/generateToken");
 const logger = require("../utils/logger");
-const { validateRegistration } = require("../utils/validation");
+const { validateRegistration, validateLogin } = require("../utils/validation");
+const RefreshToken = require("../models/RefreshToken");
 
 const registerUser = async (req, res) => {
   logger.info("Registration endpoint hit...");
@@ -52,4 +53,128 @@ const registerUser = async (req, res) => {
   }
 };
 
-module.exports = { registerUser };
+const loginUser = async (req, res) => {
+  logger.info("Login endpoint hit...");
+  try {
+    const { error } = validateLogin(req.body);
+    if (error) {
+      logger.warn("Validation error", {
+        validationMessage: error.details[0].message,
+      });
+      return res.status(400).json({
+        success: false,
+        message: error.details[0].message,
+      });
+    }
+
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      logger.warn("Invalid user");
+      return res.status(400).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+
+    // check if user password is valid
+    const passwordIsValid = await user.comparePasswords(password);
+    if (!passwordIsValid) {
+      logger.warn("Invalid password");
+      return res.status(400).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+
+    const { accessToken, refreshToken } = await generateTokens(user);
+    res.json({
+      accessToken,
+      refreshToken,
+      userId: user._id,
+    });
+  } catch (error) {
+    logger.error("Login error occured", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+// refresh token refresh
+const refreshTokenController = async (req, res) => {
+  logger.info("Refresh token endpoint hit...");
+  try {
+    const { refreshToken } = req.body;
+    if (!refreshToken) {
+      logger.warn("Refresh token missing");
+      return res.status(400).json({
+        success: false,
+        message: "Refresh token missing",
+      });
+    }
+    const storedToken = await RefreshToken.findOne({ token: refreshToken });
+    // only give new access token if refresh token still valid
+    if (!storedToken || storedToken.expiresAt < new Date()) {
+      logger.warn("Invalid or expired refresh token");
+      return res.status(401).json({
+        success: false,
+        message: "Invalid or expired refresh token",
+      });
+    }
+    const user = await User.findById(storedToken.user);
+    if (!user) {
+      logger.warn("User not found");
+      return res.status(401).json({
+        success: false,
+        message: "Refresh token user not found",
+      });
+    }
+
+    const { accessToken: newAccessToken, refreshToken: newRefreshToken } =
+      await generateTokens(user);
+
+    // delete old refresh token
+    await RefreshToken.deleteOne({ _id: storedToken._id });
+    res.json({
+      accessToken: newAccessToken,
+      refreshToken: newRefreshToken,
+    });
+  } catch (error) {
+    logger.error("Refresh token error occured", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+const logoutUser = async (req, res) => {
+  logger.info("Logout endpoint hit...");
+  try {
+    const { refreshToken } = req.body;
+    if (!refreshToken) {
+      logger.warn("Refresh token missing");
+      return res.status(400).json({
+        success: false,
+        message: "Refresh token missing",
+      });
+    }
+    await RefreshToken.deleteOne({ token: refreshToken });
+    logger.info("Refresh token deleted for logout");
+
+    res.json({
+      success: true,
+      message: "Logged out successfully",
+    });
+  } catch (error) {
+    logger.error("Error while logging out", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+module.exports = { registerUser, loginUser, refreshTokenController, logoutUser };
--- identity-service/src/routes/identityService.js
+++ identity-service/src/routes/identityService.js
@@ -1,8 +1,16 @@
 const express = require("express");
-const { registerUser } = require("../controllers/identityController");
+const {
+  registerUser,
+  loginUser,
+  refreshTokenController,
+  logoutUser,
+} = require("../controllers/identityController");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.post("/logout", logoutUser)
+router.post("/refresh-token", refreshTokenController);
 
 module.exports = router;
