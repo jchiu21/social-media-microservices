e20ddd3a77dca4e8434eedda4d497f305c95ed17
jchiu21 <jamiechiu21@gmail.com>
2025-09-16 09:28:00 -0400

    Implement media upload functionality with Cloudinary

---
+++ media-service/src/controllers/mediaController.js
@@ -0,0 +1,50 @@
+const logger = require("../utils/logger");
+const { uploadMediaToCloudinary } = require("../utils/cloudinary");
+const Media = require("../models/Media");
+
+const uploadMedia = async (req, res) => {
+  logger.info("Starting media upload");
+  try {
+    // check if file exists
+    if (!req.file) {
+      logger.error("No file found. Please add a file and try again!");
+      return res.status(400).json({
+        success: false,
+        message: "No file found. Please add a file and try again!",
+      });
+    }
+    const { originalName, mimeType, buffer } = req.file;
+    const userId = req.user.userId;
+    logger.info(`File details: name=${originalName}, type=${mimeType}`);
+    logger.info("Upload to cloudinary starting...");
+
+    // upload to cloudinary
+    const cloudinaryUploadResult = await uploadMediaToCloudinary(req.file);
+    logger.info(
+      `Cloudinary upload successful. Public Id: ${cloudinaryUploadResult.public_id}`
+    );
+    // store in mongoDB
+    const newMedia = new Media({
+      publicId: cloudinaryUploadResult.public_id,
+      originalName,
+      mimeType,
+      url: cloudinaryUploadResult.secure_url,
+      userId,
+    });
+    await newMedia.save();
+    res.status(201).json({
+      success: true,
+      mediaId: newMedia._id,
+      url: newMedia.url,
+      message: "Media upload is successful",
+    });
+  } catch (error) {
+    logger.error("Error uploading media", error);
+    res.status(500).json({
+      success: false,
+      message: "Error uploading media",
+    });
+  }
+};
+
+module.exports = { uploadMedia };
---
+++ media-service/src/utils/cloudinary.js
@@ -0,0 +1,33 @@
+const cloudinary = require("cloudinary").v2;
+const logger = require("../utils/logger");
+
+cloudinary.config({
+  cloud_name: process.env.cloud_name,
+  api_key: process.env.api_key,
+  api_secret: process.env.api_secret,
+});
+
+const uploadMediaToCloudinary = (file) => {
+  // wrap async cloudinary upload in promise so await can be used when calling this func
+  return new Promise((resolve, reject) => {
+    // create a writable stream that cloudinary can consume
+    const uploadStream = cloudinary.uploader.upload_stream(
+      {
+        resource_type: "auto", // let cloudinary detect file type automatically
+      },
+      (error, result) => {
+        // callback function
+        if (error) {
+          logger.error("Error whie uploading media to cloudinary", error);
+          reject(error); // promise fails if error
+        } else {
+          resolve(result); // promise succeeds with result
+        }
+      }
+    );
+    // take file buffer (raw binary data) and write to cloudinary stream
+    uploadStream.end(file.buffer);
+  });
+};
+
+module.exports = { uploadMediaToCloudinary };
